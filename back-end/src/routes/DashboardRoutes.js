fb451d82d0af394cc0eec83151e94e6569675b7c
SMGuilherme07 <guimsoares_ita@hotmail.com>
2026-07-16 19:09:50 -0300

    created migration.sql

--- back-end/src/routes/DashboardRoutes.js
+++ back-end/src/routes/DashboardRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getDashboardData } from '../controllers/DashboardController.js'
+import { getDashboardData, repla } from '../controllers/DashboardController.js'
 
 const router = Router();
 
 router.get('/', getDashboardData);
+router.post('/replacement', replacement);
 
 export default router;
