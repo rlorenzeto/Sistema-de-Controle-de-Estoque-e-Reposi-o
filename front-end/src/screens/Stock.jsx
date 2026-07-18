269175103a889639b07c6db58896305e01b09642
Rogério <rogerio.nascf@gmail.com>
2026-07-17 23:13:05 -0300

    Página Estoque ok

--- front-end/src/screens/Stock.jsx
+++ front-end/src/screens/Stock.jsx
@@ -1,28 +1,44 @@
+import { useState } from "react";
 import "./Stock.css"
 import SideBar from '../components/SideBar'
 import HeaderButton from '../components/HeaderButton';
+import CardResumo from "../components/cardResumo";
+import TabelaEstoque from "../components/TabelaEstoque";
+import { FiSearch } from "react-icons/fi";
+import OrderModal from "../components/OrderModal";
+
 
 export default function Stock(){
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleNovoItem = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  
     return(
         <>
             <div className="stock-container">
                 <SideBar/>
                 <div className="stock-panel">
-                <HeaderButton title="Estoque" button="+ Novo Item"/>
-                <section className="informative">
-                    <div className="box-one">
-                        <h3>Produtos Cadastrados</h3>
-                        <p>300</p>
+                <HeaderButton title="Estoque" button="+ Novo Item"
+                onButtonClick={handleNovoPedido}
+                />
+                
+                <main className="stock-main">
+                    <div className="stock-cards">
+                    <CardResumo title="Produtos Cadastrados" value="300" />
+                    <CardResumo title="Produtos em Alerta" value="10" />
+                    <CardResumo title="Produtos em Crítico" value="2" />
                     </div>
-                    <div className="box-two">
-                        <h3>Produtos em Alerta</h3>
-                    </div>
-                    <div className="box-three">
-                        <h3>Produtos em Crítico</h3>
-                    </div>
-                </section>
-                </div>
+                    <TabelaEstoque/>
+                </main>
+                </div>               
             </div>
+            <OrderModal isOpen={isModalOpen} onClose={handleCloseModal} />
         </>
     );
 }
