653b3c0a1d081e126438b6fd1c8b4fabe9d98127
SMGuilherme07 <guimsoares_ita@hotmail.com>
2026-07-20 20:58:51 -0300

    fix routes

--- back-end/src/routes/PerfilRoutes.js
+++ back-end/src/routes/PerfilRoutes.js
@@ -4,6 +4,6 @@ import { changePassword, getUser } from "../controllers/PerfilController.js"
 const router = Router();
 
 router.get('/get-user', getUser);
-router.put('change-password', changePassword)
+router.put('/change-password', changePassword)
 
 export default router;
